ca366f22b18c5126b658940584a043887812d630
Entropy Xu <entropy@EntropyXuMacbook.local>
2019-05-06 01:38:41 -0700

    Add support for String Constructor for Toxic_Instruction

--- src/entropy/xu/Toxic_Instruction.java
+++ src/entropy/xu/Toxic_Instruction.java
@@ -66,10 +66,78 @@ public class Toxic_Instruction {
         }
     }
 
-    public Toxic_Instruction(String str)
+    public Toxic_Instruction(String asm)
     {
-        this(new Toxic_Register(str));
+        switch (asm)
+        {
+            case "P0":
+                type = Ins_Type.P0;
+                bits = new Toxic_Register("0000");
+                break;
+            case "P1":
+                type = Ins_Type.P1;
+                bits = new Toxic_Register("0001");
+                break;
+            case "CMP":
+                type = Ins_Type.CMP;
+                bits = new Toxic_Register("0011");
+                break;
+            case "PC":
+                type = Ins_Type.PC;
+                bits = new Toxic_Register("0010");
+                break;
+            case "POP":
+                type = Ins_Type.POP;
+                bits = new Toxic_Register("0100");
+                break;
+            case "DIS":
+                type = Ins_Type.DIS;
+                bits = new Toxic_Register("0101");
+                break;
+            case "SWP":
+                type = Ins_Type.SWP;
+                bits = new Toxic_Register("0111");
+                break;
+            case "RVS":
+                type = Ins_Type.RVS;
+                bits = new Toxic_Register("0110");
+                break;
+            case "ADD":
+                type = Ins_Type.ADD;
+                bits = new Toxic_Register("1100");
+                break;
+            case "NAND":
+                type = Ins_Type.NAND;
+                bits = new Toxic_Register("1101");
+                break;
+            case "LS":
+                type = Ins_Type.LS;
+                bits = new Toxic_Register("1111");
+                break;
+            case "RS":
+                type = Ins_Type.RS;
+                bits = new Toxic_Register("1110");
+                break;
+            case "SV":
+                type = Ins_Type.SV;
+                bits = new Toxic_Register("1000");
+                break;
+            case "LD":
+                type = Ins_Type.LD;
+                bits = new Toxic_Register("1001");
+                break;
+            case "B1":
+                type = Ins_Type.B1;
+                bits = new Toxic_Register("1011");
+                break;
+            case "B0":
+                type = Ins_Type.B0;
+                bits = new Toxic_Register("1010");
+                break;
+                default: throw new IllegalArgumentException("No Corresponding Instruction");
+        }
     }
+
 
     @Override
     public String toString() {
